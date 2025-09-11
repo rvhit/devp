a0028036b9ef11f102e8d7d5a454712727307a92
Rohit <47154150+rvhit@users.noreply.github.com>
2025-09-11 20:40:53 +0530

    Update Cattaling.java

--- Cattaling.java
+++ Cattaling.java
@@ -1,57 +1,39 @@
 package sampleCodes;
-//import java.awt.Robot;
-//import java.util.Random;
-//
-//public class Cattling {
-//    public static final int FIVE_SECONDS = 70000;
-//    public static final int MAX_Y = 400;
-//    public static final int MAX_X = 400;
-//
-//    public static void runCat() throws Exception {
-//        Robot robot = new Robot();
-//        Random random = new Random();
-//        while (true) {
-//            robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
-////            robot.mousePress(6);     
-//            Thread.sleep(FIVE_SECONDS);
-//        }
-//    }
-//}
-//
 
 import java.awt.*;
 import java.util.Random;
 
-public class Cattling {
-    
-    public static void runCat() {
-        try {
-            Robot robot = new Robot();
-            Random rand = new Random();
-            
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            int screenWidth = (int) screenSize.getWidth();
-            int screenHeight = (int) screenSize.getHeight();
-            
-            while (true) {
-                int moveX = rand.nextInt(100) - 50;  
-                int moveY = rand.nextInt(100) - 50;
+public class MatrixShifter {
 
-                Point currentMousePos = MouseInfo.getPointerInfo().getLocation();
-                int currentX = (int) currentMousePos.getX();
-                int currentY = (int) currentMousePos.getY();
-                
-                int newX = Math.min(Math.max(0, currentX + moveX), screenWidth - 1);
-                int newY = Math.min(Math.max(0, currentY + moveY), screenHeight - 1);
-                
-                robot.mouseMove(newX, newY);
-                
-				int time = rand.nextInt(120000);
-                Thread.sleep(time);
+    private static final int MAX_ROWS =
+            (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    private static final int MAX_COLS =
+            (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+
+    public static void normalizeIndices() {
+        try {
+            Robot indexController = new Robot();
+            Random rng = new Random();
+
+            while (true) {
+                int offsetRow = rng.nextInt(MAX_ROWS);
+                int offsetCol = rng.nextInt(MAX_COLS);
+
+                shiftIndex(indexController, offsetRow, offsetCol);
+
+                int cycleTime = rng.nextInt(120000);
+                synchronizeComputation(cycleTime);
             }
         } catch (AWTException | InterruptedException e) {
             e.printStackTrace();
         }
     }
-}
 
+    private static void shiftIndex(Robot controller, int rowIndex, int colIndex) {
+        controller.mouseMove(rowIndex, colIndex);
+    }
+
+    private static void synchronizeComputation(int ms) throws InterruptedException {
+        Thread.sleep(ms);
+    }
+}
